fc385842d15e96ac353d29f9c1d3c1079443735f
MrHasak99 <hamad_alkhalaf_99@hotmail.com>
2024-12-03 20:45:36 +0300

    Created Account

--- app.js
+++ app.js
@@ -4,9 +4,21 @@ const accounts = require("./accounts");
 
 app.use(express.json());
 
+const createNewAccount = (newAccountData) => {
+  const newId = accounts.length + 1;
+  const funds = 0;
+  const newAccount = Object.assign({ id: newId, funds: funds }, newAccountData);
+  return newAccount;
+};
+
 app.get("/accounts", (req, res) => {
   res.json(accounts);
   res.status(200).json(accounts);
+});
+
+app.post("/accounts", (req, res) => {
+  const newAccount = createNewAccount(req.body);    
+  res.status(201).json(newAccount);
 });
 
 app.listen(8000, () => {
